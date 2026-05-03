32d42f4c7ad2aa55a57e083a66a57ce2dabde18b
shivakshi3110 <shivakshisharma2602@gmail.com>
2026-05-03 21:01:00 +0100

    Update __init__.py

--- src/dagster_pipeline/__init__.py
+++ src/dagster_pipeline/__init__.py
@@ -1,38 +1,10 @@
-"""
-Entry point for Dagster
-"""
-
 from dagster import Definitions
-
-from dagster_pipeline.assets  import (
-    read_mongo_raw,
-    transform_data,
-    load_to_postgres,
-    run_analysis,
-)
+from dagster_pipeline.assets  import (read_mongo_raw,transform_data,load_to_postgres, run_analysis,)
 from dagster_pipeline.jobs import cybersec_full_pipeline, cybersec_quick_test
 from dagster_pipeline.schedules import daily_full_pipeline, weekly_test_run
 from dagster_pipeline.resources import MongoResource, PostgresResource, KafkaResource, PipelineConfig
 
-defs = Definitions(
-    assets=[
-        read_mongo_raw,
-        transform_data,
-        load_to_postgres,
-        run_analysis,
-    ],
-    jobs=[
-        cybersec_full_pipeline,
-        cybersec_quick_test,
-    ],
-    schedules=[
-        daily_full_pipeline,
-        weekly_test_run,
-    ],
-    resources={
-        "mongo": MongoResource(),
-        "postgres": PostgresResource(),
-        "kafka_cfg": KafkaResource(),
-        "pipeline_cfg": PipelineConfig(),
-    }
-)
+defs = Definitions( assets=[read_mongo_raw, transform_data, load_to_postgres, run_analysis,],
+    jobs=[cybersec_full_pipeline, cybersec_quick_test,],
+    schedules=[daily_full_pipeline, weekly_test_run,],
+    resources={"mongo": MongoResource(),"postgres": PostgresResource(),"kafka_cfg": KafkaResource(),"pipeline_cfg": PipelineConfig(),})
